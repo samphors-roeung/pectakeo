89af3ff197274e5ee2b60fd641e74990b0a3dad5
samphors-roeung <your-email@example.com>
2026-09-19 19:18:05 +0700

    fix(vercel): use export default in api/gas.js for type: module

--- api/gas.js
+++ api/gas.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -63,4 +63,4 @@ module.exports = async (req, res) => {
       message: "Proxy Error: " + error.message
     });
   }
-};
+}
